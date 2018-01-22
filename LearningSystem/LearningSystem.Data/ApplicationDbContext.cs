6a19f082968cf68adcdd9e376040bbb082c054ae
Lachezar Lalov <luchoazz@abv.bg>
2018-01-22 14:27:06 +0200

    initial commit

---
+++ LearningSystem/LearningSystem.Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿namespace LearningSystem.Data
+{
+    using LearningSystem.Data.Models;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+    
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
